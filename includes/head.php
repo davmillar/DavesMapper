2c8578180231467a43759d1967f7845ba851837b
David Millar <dave@davegoesthedistance.com>
2017-03-18 23:11:35 -0500

    Metadata cleanup.

--- includes/head.php
+++ includes/head.php
@@ -1,29 +1,38 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=10,chrome=1">
 
+<!-- Application Title -->
 <meta name="application-name" content="Dave's Mapper">
 <meta itemprop="name" content="Dave's Mapper">
+<meta name="apple-mobile-web-app-title" content="Dave's Mapper">
 
+<!-- Description of App -->
 <meta name="msapplication-tooltip" content="Geomorphic RPG Map Generator">
 <meta itemprop="description" content="Geomorphic map generator web app for role-playing enthusiasts. Created by web designer and puzzle author David Millar.">
 
+<!-- App Main URL -->
 <meta name="application-url" content="https://davesmapper.com">
 <meta name="msapplication-starturl" content="https://davesmapper.com">
 
+<!-- App Window Sizing -->
 <meta name="msapplication-window" content="width=1024;height=768">
 
+<!-- App Capability/Manifest -->
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="mobile-web-app-capable" content="yes">
 <link rel="manifest" href="/manifest.json">
 
+<!-- App Status Bar Style -->
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
+<!-- App Viewport -->
 <meta name="viewport" content="user-scalable=no, maximum-scale=1, minimum-scale=1, initial-scale=1, width=device-width">
 
+<!-- App Color Scheme -->
 <meta name="msapplication-navbutton-color" content="#aa6e33">
 <meta name="theme-color" content="#aa6e33">
 
-<link rel="publisher" href="https://plus.google.com/107206980364478824224">
+<!-- App Icons -->
 <link href="/m_icons/favicon.ico" rel="shortcut icon">
 <meta itemprop="image" content="https://davesmapper.com/assets/images/logo-300.png">
 <link rel="apple-touch-icon-precomposed" href="/assets/images/logo-300.png">
@@ -33,7 +42,12 @@
 <link rel="shortcut icon" sizes="300x300" href="/assets/images/logo-300.png">
 <link rel="shortcut icon" sizes="196x196" href="/assets/images/logo-196.png">
 <link rel="shortcut icon" sizes="48x48" href="/assets/images/logo-48.png">
+<meta name="msapplication-square310x310logo" content="/assets/images/logo-600.png">
 
+<!-- App Publisher -->
+<link rel="publisher" href="https://plus.google.com/107206980364478824224">
+
+<!-- Assets -->
 <link href="//fonts.googleapis.com/css?family=Philosopher:regular,bold" rel="stylesheet">
 <link href="/assets/css/compiled.css?t=<?php echo filemtime(PATH . "/assets/css/compiled.css")?>" rel="stylesheet" media="screen">
 <link href="/assets/css/compiled_print.css?t=<?php echo filemtime(PATH . "/assets/css/compiled_print.css")?>" rel="stylesheet" media="print">

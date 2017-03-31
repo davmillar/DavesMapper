dd268d9588263d33961dbe1ac20f77d4581406cd
David Millar <dave@davegoesthedistance.com>
2017-03-30 20:16:48 -0500

    Fixes to service worker logic. and handling.

--- assets-src/js/service-worker.js
+++ assets-src/js/service-worker.js
@@ -33,3 +33,17 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches['delete'](cacheName);
+          }
+        })
+      );
+    })
+  );
+});
--- build.sh
+++ build.sh
@@ -65,10 +65,11 @@ jssum1="$(md5sum assets/js/compiled_app.js | cut -c -5)"
 jssum2="$(md5sum assets/js/global.js | cut -c -5)"
 csssum1="$(md5sum assets/css/compiled.css | cut -c -5)"
 csssum2="$(md5sum assets/css/compiled_print.css | cut -c -5)"
+indexsum="$(md5sum index.php | cut -c -5)"
 
 cat \
   assets-src/js/service-worker.js \
-  | sed "s/my-site-cache-v1/${jssum1}-${jssum2}-${csssum1}-${csssum2}/" \
+  | sed "s/my-site-cache-v1/${jssum1}-${jssum2}-${csssum1}-${csssum2}-${indexsum}/" \
   | java -jar bin/yuicompressor-2.4.8.jar --type=js \
   > ./assets/js/service-worker.js
 

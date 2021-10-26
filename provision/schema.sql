f4afdef6840c7d48f0eb2b8b8e5692681354fa20
David Millar <dave@davegoesthedistance.com>
2021-10-25 20:18:12 -0500

    Added the schema for the current database structure.

--- provision/schema.sql
+++ provision/schema.sql
@@ -1,1 +1,52 @@
--- TODO: Commit schema-only table structure to this file for first-run setup.
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+CREATE TABLE `artists` (
+  `id` int(11) NOT NULL,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `url_slug` text COLLATE utf8_unicode_ci NOT NULL,
+  `initials` text COLLATE utf8_unicode_ci NOT NULL,
+  `icon` text COLLATE utf8_unicode_ci NOT NULL,
+  `url` text COLLATE utf8_unicode_ci NOT NULL,
+  `bio` text COLLATE utf8_unicode_ci NOT NULL,
+  `email` text COLLATE utf8_unicode_ci NOT NULL,
+  `password` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `tiles` (
+  `id` int(11) NOT NULL,
+  `image` varchar(70) COLLATE utf8_unicode_ci NOT NULL,
+  `tile_type` int(11) NOT NULL,
+  `map_type` int(11) NOT NULL,
+  `artist_id` int(11) NOT NULL,
+  `has_exit` tinyint(1) NOT NULL,
+  `approved` tinyint(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+ALTER TABLE `artists`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `tiles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `image` (`image`);
+
+
+ALTER TABLE `artists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `tiles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

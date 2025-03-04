f07644937cd5612d08606890ca6ee14b7deb31f1
David Millar <dave@davegoesthedistance.com>
2025-03-04 00:16:06 -0600

    Fix security issue.

--- scripts/load_authors.php
+++ scripts/load_authors.php
@@ -7,7 +7,7 @@
     $map_type = ($map_type > 0) ? $map_type : 1;
     $map_type_phrase = ($map_type == 3) ? "IN (1,2,3)" : "= ".$map_type;
 
-  $data = $pdo->query("SELECT * FROM artists a LEFT JOIN
+  $data = $pdo->query("SELECT a.id, a.name, a.url_slug, a.initials, a.url, a.bio, t.tiles, t.artist_id FROM artists a LEFT JOIN
     (SELECT COUNT(id) AS tiles, artist_id FROM tiles
      WHERE map_type ".$map_type_phrase." AND approved = 1 GROUP BY artist_id) t
     ON a.id = t.artist_id ORDER BY a.name");
